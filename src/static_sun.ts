8b25d94d2ed86e6761ba296e8ae6dfc81ef6c991
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2025-03-28 12:02:11 -0400

    Allow static_sun to use planes

--- src/static_sun.ts
+++ src/static_sun.ts
@@ -2,7 +2,8 @@ import { Group, Object3D } from "three";
 import { Quality, QualitySettings } from "./quality";
 import { Database, SunTextureData } from "./database";
 import { CreateTextures, SunTexture } from "./texture";
-import { CreateSphericalModel, UpdateModelOpacity } from "./model_builder";
+import { CreateSphericalModel, UpdateModelOpacity, CreatePlaneWithTexture } from "./model_builder";
+import { PLANE_SOURCES } from "./sourceinfo";
 
 /**
  * The static sun is a sun that represents 1 image instead
@@ -27,10 +28,11 @@ class StaticSun extends Object3D {
         this._time = date;
         this.ready = new Promise(async (resolve, reject) => {
           this.imageData = await Database.GetImage(source, date, quality);
-          console.log(`Loading sun ${source} with timestamp ${this.imageData.date.toISOString()}`);
           this.texture = (await CreateTextures([this.imageData]))[0];
           this._time = this.texture.date;
-          this.model = await CreateSphericalModel(this.texture.texture, this.imageData.jp2Metadata, this.imageData.jp2info);
+          this.model = PLANE_SOURCES.indexOf(source) === -1 ?
+            await CreateSphericalModel(this.texture.texture, this.imageData.jp2Metadata, this.imageData.jp2info) :
+            await CreatePlaneWithTexture(this.texture.texture, this.imageData.jp2info);
           this.add(this.model);
           resolve();
         });
